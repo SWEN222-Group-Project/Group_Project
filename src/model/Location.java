112300c63f88b87d8728b5f5b1817dcfae76f86a
Harmannz <harman@harmans-mbp.lan>
2015-09-17 16:07:22 +1200

    Updated Position class
    Position class has methods that now allow adjacent location/cells to be
    retrieved. This will allow game to determine adjacent empty space and
    also allows to determine what is placed in adjacent cells to the player

--- src/model/Location.java
+++ src/model/Location.java
@@ -1,5 +1,29 @@
 package model;
 
 public class Location {
-//has x and y index for 2d array of pieces in 
+
+	private int xPos, yPos;
+	
+	public Location(int xPos, int yPos){
+		this.xPos = xPos;
+		this.yPos = yPos;
+	}
+
+	public int getxPos() {
+		return xPos;
+	}
+
+	public int getyPos() {
+		return yPos;
+	}
+
+	public void setxPos(int xPos) {
+		this.xPos = xPos;
+	}
+
+	public void setyPos(int yPos) {
+		this.yPos = yPos;
+	}
+	
+	
 }
--- src/model/Position.java
+++ src/model/Position.java
@@ -7,9 +7,53 @@ package model;
 public class Position {
 	private Room room;
 	private Location location;
-	private int xPos, yPos;
+	
+	public Position(Room room, Location location){
+		this.room = room;
+		this.location = location;
+	}
 	
 	public Room getRoom(){
 		return this.room;
+	}
+	
+	public Location getLocation(){
+		return this.location;
+	}
+	
+	public void setLocation(Location loc){
+		this.location = new Location(loc.getxPos(), loc.getyPos());
+	}
+	
+	/**
+	 * Gets the location north of current location.
+	 * @return 
+	 */
+	public Location getNorth(){
+		return new Location(location.getxPos(), location.getyPos() - 1);
+	}
+	
+	/**
+	 * Gets the location east of current location.
+	 * @return 
+	 */
+	public Location getEast(){
+		return new Location(location.getxPos() + 1, location.getyPos());
+	}
+	
+	/**
+	 * Gets the location south of current location.
+	 * @return 
+	 */
+	public Location getSouth(){
+		return new Location(location.getxPos(), location.getyPos() + 1);
+	}
+	
+	/**
+	 * Gets the location west of current location.
+	 * @return 
+	 */
+	public Location getWest(){
+		return new Location(location.getxPos() - 1, location.getyPos());
 	}
 }
