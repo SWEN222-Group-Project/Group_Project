49d5d30b6b9f65722b56f2828d60c55430d915f4
Harmannz <harman@harmans-mbp.lan>
2015-09-18 21:06:53 +1200

    Removed getEast/West/North/South methods.
    These methods have been shifted to Location class and are no longer
    required in the Position class.

--- src/model/Position.java
+++ src/model/Position.java
@@ -18,42 +18,10 @@ public class Position {
 	}
 	
 	public Location getLocation(){
-		return this.location;
+		return new Location(location.getxPos(), location.getyPos());
 	}
 	
 	public void setLocation(Location loc){
 		this.location = new Location(loc.getxPos(), loc.getyPos());
-	}
-	
-	/**
-	 * Gets the location north of current location.
-	 * @return 
-	 */
-	public Location getNorth(){
-		return new Location(location.getxPos(), location.getyPos() - 1);
-	}
-	
-	/**
-	 * Gets the location east of current location.
-	 * @return 
-	 */
-	public Location getEast(){
-		return new Location(location.getxPos() + 1, location.getyPos());
-	}
-	
-	/**
-	 * Gets the location south of current location.
-	 * @return 
-	 */
-	public Location getSouth(){
-		return new Location(location.getxPos(), location.getyPos() + 1);
-	}
-	
-	/**
-	 * Gets the location west of current location.
-	 * @return 
-	 */
-	public Location getWest(){
-		return new Location(location.getxPos() - 1, location.getyPos());
 	}
 }
