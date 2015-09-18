f5d2787e6394a2c9b323b51846de5d54f6c67eba
Harmannz <harman@harmans-mbp.lan>
2015-09-18 21:09:16 +1200

    Added 2d array of Pieces to Room class..
    Now a Room instance contains a 2D array of Pieces that represent the
    internal structure of the Room. Piece, such as Player or items, can be
    added to specific locations in the 2D array. 
    To allow pieces to be added and removed, respective methods have been
    included.
    Also, printRoom() method has been added to allow a room to be printed on
    the console for testing.

--- src/model/Room.java
+++ src/model/Room.java
@@ -1,13 +1,48 @@
 package model;
 
 public class Room {
-    private Piece item;
+	public final static int ROOM_WIDTH = 9; 
+    private Piece[][] board; //underlying structure of each room
+    private String name;
     
-    public Piece getItem(){
-    	return item;
+    public Room(String name){
+    	this.board = new Piece[ROOM_WIDTH][ROOM_WIDTH];
+//    	addWalls();
+    	this.name = name;
     }
     
-    public void removeItem(){
-    	this.item = null;
+    public Room(Piece[][] board){
+    	this.board = board;
+    }
+    
+    public void addPiece(Location loc, Piece piece){
+    	board[loc.getyPos()][loc.getxPos()] = piece;
+    }
+    
+    public Piece getPiece(Location loc){
+    	return board[loc.getyPos()][loc.getxPos()];
+    }
+    
+    public Piece removePiece(Location loc){
+    	Piece piece = board[loc.getyPos()][loc.getxPos()];
+    	board[loc.getyPos()][loc.getxPos()] = null;
+    	return piece;
+    }
+    
+    public void printRoom(){
+    	for(int i = 0; i < ROOM_WIDTH; i++){
+			System.out.print("|");
+			for(int j = 0; j < ROOM_WIDTH; j++){
+				if(board[i][j] != null)
+					System.out.print(board[i][j].toString() + "|");
+				else
+					System.out.print("_|");
+			}
+			System.out.println();
+		}    	
+    }
+    
+    public String toString(){
+    	return name;
     }
 }
