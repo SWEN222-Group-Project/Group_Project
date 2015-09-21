89026bbf0d4f9af63ee55347fe6e1f92217dab68
Harmannz <harman@harmans-mbp.lan>
2015-09-21 19:35:28 +1200

    Updated constructor to comply with the Piece class

--- src/model/NonMovablePiece.java
+++ src/model/NonMovablePiece.java
@@ -6,9 +6,22 @@ package model;
  */
 public abstract class NonMovablePiece extends Piece {
 
-	public NonMovablePiece(Position position) {
-		super(position);
+	
+	
+	public NonMovablePiece(Position position, String name, String description) {
+		super(position, name, description);
 		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void setPosition(Position position){
+		//throw exception as we cannot change position
+		System.out.println("Cannot move NonMovablePieces");
+	}
+	
+	@Override
+	public void setLocation(Location loc){
+		//throw exception as we cannot change location
 	}
 
 }
