2396c538f5b31a10f6ed4e92756926a987174f93
Harmannz <harman@harmans-mbp.lan>
2015-09-21 19:33:20 +1200

    Updated constructor to comply with changes to Piece class

--- src/model/MovablePiece.java
+++ src/model/MovablePiece.java
@@ -1,14 +1,14 @@
 package model;
 
 /**
- * Represents movable pieces inside rooms	
+ * Represents movable pieces inside rooms. This might just be a dummy class.	
  * @author harman
  *
  */
 public abstract class MovablePiece extends Piece {
 
-	public MovablePiece(Position position) {
-		super(position);
+	public MovablePiece(Position position, String name, String description) {
+		super(position, name, description);
 		// TODO Auto-generated constructor stub
 	}
 
