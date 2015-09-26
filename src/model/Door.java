8d8231e135cbe666fe35b562742f7912f6a88f35
Harmannz <harman@10.140.113.159>
2015-09-26 15:11:14 +1200

    Created Door
    This object represents a door that connects two rooms

---
+++ src/model/Door.java
@@ -0,0 +1,13 @@
+package model;
+
+public class Door extends Piece {
+	
+	//TODO: have a Position object that the door leads to
+	//
+	public Door(Position position, String name, String description,
+			Direction direction) {
+		super(position, name, description, direction);
+		// TODO Auto-generated constructor stub
+	}
+
+}
