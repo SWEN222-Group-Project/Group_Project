8203818b4c0c04c3d67bc54377bd920c8eecf674
Harmannz <harman@10.140.113.159>
2015-09-26 15:14:34 +1200

    Created Item
    This class represents the abstract notation of an item in the game.
    The item is anything that the player may communicate with during the
    game, such as Coin, Chest, Assignment etc.

---
+++ src/model/Item.java
@@ -0,0 +1,22 @@
+package model;
+
+public abstract class Item extends Piece {
+
+	public Item(Position position, String name, String description,
+			Direction direction) {
+		super(position, name, description, direction);
+		// TODO Auto-generated constructor stub
+	}
+	
+	//item has a description
+	//examine method that calls the description of item (remember composite has to call examine of all items)
+	//need to add all items to the player: composite has to add =
+	/**
+	 * add item to player
+	 * @param player
+	 */
+	public abstract void addTo(Player player, Location location);
+	
+	
+	//setPosition(Position) sets the position of all items in Item object. if nonMovable then throw error  
+}
