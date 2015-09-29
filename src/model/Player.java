38120ddb1b5de70b40860192d0759640b7b2ae52
Harmannz <harman@harmans-mbp.lan>
2015-09-29 23:09:30 +1300

    Added Points and Assignments collection
    The player can has a set of assignments that they have to collect in the
    game. This allows the player to collect assignments to win the game.
    Points are incremented when a player picks up a coin.

--- src/model/Player.java
+++ src/model/Player.java
@@ -1,10 +1,15 @@
 package model;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.SortedSet;
+import java.util.TreeSet;
 
 public class Player extends Piece {
 	public static final int MAX_ITEMS = 8;
 	private List<Piece> container;
-//	private int points
+	private SortedSet<Assignment> assignments;
+	private int points;
+	
 	//Player ID: this can be used to access the player when it is in a Game
 	private int id;
 	
@@ -12,19 +17,41 @@ public class Player extends Piece {
 		super(pos, name, null,direction);
 		this.id = playerId;
 		this.container = new ArrayList<Piece>();
+		this.assignments = new TreeSet<Assignment>();
+		this.points = 0;
 		
 	}
 	
+	public int points(){
+		return points;
+	}
+	public boolean hasWon(){
+		return assignments.size() == Game.MAX_ASSIGN;
+	}
+	public void addPoint(int i){
+		this.points+= i;
+	}
 
 	public int id(){
 		return id;
 	}
 
+	public boolean addAssign(Assignment assign){
+		if(assignments.size() == Game.MAX_ASSIGN){
+			return false;
+		}
+		return assignments.add(assign);
+	}
+	
+	public int numOfAssign(){
+		return assignments.size();
+	}
+	
 	public boolean addItem(Piece p){
 		if(container.size() <= MAX_ITEMS){
 		//if full then throw exception
 		//TODO: need a addItems method in Piece class that gets a list of
-		//first 
+		//first
 			this.container.add(p);
 			return true;
 		}else{
@@ -39,6 +66,7 @@ public class Player extends Piece {
 	public boolean removeItem(Piece p){
 		//need to find piece and then remove it
 		//TODO: will need to implement equals and hashcode for the piece class
+		assignments.remove(p);
 		return container.remove(p);
 	}
 	
@@ -47,7 +75,7 @@ public class Player extends Piece {
 	}
 	
 	public boolean hasItems(){
-		return container.isEmpty();
+		return !container.isEmpty();
 	}
 	
 	public String toString(){
