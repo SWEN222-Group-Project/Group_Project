e347222022a5cf40f6e44ef6100cfaa12679485d
Harmannz <harman@harmans-mbp.lan>
2015-09-29 23:17:52 +1300

    Removed Wall interface
    Since the door and Wall class are treated as different objects, the
    interface has been replaced by a concreate wall class

--- src/model/Wall.java
+++ src/model/Wall.java
@@ -5,6 +5,12 @@ package model;
  * @author harman
  *
  */
-public interface Wall {
+public class Wall extends Piece{
+
+	public Wall(Position position,
+			Direction direction) {
+		super(position, "Wall", null, direction);
+		// TODO Auto-generated constructor stub
+	}
 	//have a drawImage method that have to be implemented by the wallImpl and door class
 }
