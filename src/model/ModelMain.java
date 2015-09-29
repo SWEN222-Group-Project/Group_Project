36f43f088230610304025ebab510b8327b7633f5
Harmannz <harman@harmans-mbp.lan>
2015-09-29 23:05:48 +1300

    Updated the Test class to reflect changes
    Running the main method of this class will allow you to test and see the
    features of the current model. The console will print out the room.
    Movement of the player can be tested and seen inside the room, picking
    up items can be viewed and also dropping item. 
    
    There are now two rooms and a player has to pickup the key before going
    to the other room

--- src/model/ModelMain.java
+++ src/model/ModelMain.java
@@ -33,24 +33,24 @@ public class ModelMain {
 		int playerId = p.id();
 		while(true){
 			Location location = p.getPosition().getLocation();
-			
+			Room room = p.getRoom();
 			String cmd = inputString("[w/a/s/d/q/l/f] or any key to exit").toLowerCase();
 			if (cmd.equals("w")){
 				//go up
 				System.out.println(location.getNorth());
-				game.movePlayer(playerId, location.getNorth());
+				game.movePlayer(playerId, location.getNorth(), room);
 			}else if (cmd.equals("a")){
 				//go left
 				System.out.println(location.getWest());
-				game.movePlayer(playerId, location.getWest());
+				game.movePlayer(playerId, location.getWest(), room);
 			} else if (cmd.equals("s")){
 				//go down
 				System.out.println(location.getSouth());
-				game.movePlayer(playerId, location.getSouth());
+				game.movePlayer(playerId, location.getSouth(), room);
 			} else if (cmd.equals("d")){
 				//go right
 				System.out.println(location.getEast());
-				game.movePlayer(playerId, location.getEast());
+				game.movePlayer(playerId, location.getEast(), room);
 			} else if (cmd.equals("q")){
 				//go right
 				game.dropItem(playerId, dropPiece);
@@ -75,26 +75,39 @@ public class ModelMain {
 	
 	public static void main(String[] args){
 		//create room
+		Game game = new Game();
 		Room room = new Room("Practise Room");
+		Room room2 = new Room("Second Room");
 		Position pos1 = new Position(room, new Location(1,1));
 		Position pos2 = new Position(room, new Location(7,4));
+		Position pos3 = new Position(room, new Location(7,7));
+		Door door = new Door(new Position(room, new Location(0,0)), Direction.SOUTH, new Position(room2, new Location(8,8)));
+		
+		room.addDoor(door);
 		Player p1 = new Player(1, "1Harman", pos1, Direction.NORTH);
-//		Player p2 = new Player(2, "2Harman", pos2, Direction.SOUTH);
+		Player p2 = new Player(2, "2Harman", new Position(room, new Location(4,5)), Direction.SOUTH);
 		Coin coin = new Coin(pos2, "C", "Coin", Direction.EAST);
 		ItemsComposite chest = new ItemsComposite(pos2, "h", "Chest",Direction.NORTH);
-		chest.addStrategy(new NonMovableStrategy());
+		chest.addStrategy(new MovableStrategy());
 		chest.addItem(coin);
+//		Coin coin2 = new Coin(pos3, "C2", "Coin", Direction.EAST);
+//		chest.addItem(coin2);
+		Key key = new Key(new Position(room, new Location(1,0)), 0);
+		door.addKey(key);
+		game.addPiece(key, new Position(room, new Location(1,0)));
+		Assignment a1 = new Assignment(pos3, 0, null);
+		game.addPiece(a1, pos3);
 		
 		
-		
-		Game game = new Game();
 
 		game.addPiece(chest, pos2);
 		game.addRoom(room); //test adding room
+		game.addRoom(room2);
 		game.addPlayer(p1); //test adding player
-//		game.addPlayer(p2);
+		game.addPlayer(p2);
 		game.printAll(); //this will print all of the rooms
 		Control controller = new Control(game);
 		movePlayer(controller, p1, coin); //test moving player
+		movePlayer(controller, p2, coin);
 	}
 }
