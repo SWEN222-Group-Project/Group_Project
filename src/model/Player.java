fd2a474fed3e9a806440f0d31f6bc5bebe037f20
Harmannz <harman@harmans-mbp.lan>
2015-10-06 11:33:40 +1300

    Added byte array functionality for Player
    The player object can contain multiple pieces so the inputStream and
    outputStream methods allow the player state and all of the items the
    player contains to be converted into a byte array for allowing the
    client's to load the game.

--- src/model/Player.java
+++ src/model/Player.java
@@ -1,4 +1,7 @@
 package model;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.SortedSet;
@@ -14,7 +17,7 @@ public class Player extends Piece {
 	private int id;
 	
 	public Player (int playerId, String name, Position pos, Direction direction){
-		super(pos, name, null,direction);
+		super(pos, name, "" ,direction);
 		this.id = playerId;
 		this.container = new ArrayList<Piece>();
 		this.assignments = new TreeSet<Assignment>();
@@ -41,6 +44,10 @@ public class Player extends Piece {
 			return false;
 		}
 		return assignments.add(assign);
+	}
+	
+	public void addAssigns(List<Assignment> assigns){
+		this.container.addAll(assigns);
 	}
 	
 	public int numOfAssign(){
@@ -80,5 +87,102 @@ public class Player extends Piece {
 	
 	public String toString(){
 		return "" + id;
+	}
+
+	public static Player fromInputStream(
+			DataInputStream din, String name, Position pos) throws IOException{
+	
+		Direction dir = Direction.values()[din.readByte()];
+		
+		int id = din.readInt();
+		int points = din.readByte();
+		
+		int numContain = din.readByte();
+//		assert(numContain == 0);
+		List<Piece> container = new ArrayList<Piece>();
+		for(int i = 0; i < numContain; i++){
+			container.add(Piece.fromInputStream(din, null));
+		}
+		
+		int numAssign= din.readByte();
+		List<Piece> assignments = new ArrayList<Piece>();
+		for(int i = 0; i < numAssign; i++){
+			assignments.add(Piece.fromInputStream(din, null));
+		}
+		
+		Player p =  new Player(id, name, pos, dir);
+		p.addItems(container);
+		p.addPoint(points);
+		
+		for(Piece assign: assignments){
+			if(assign instanceof Assignment){
+				p.addAssign((Assignment) assign); //safe
+			}
+		}
+		return p;		
+	}
+	
+	private static Room getRoom(List<Room> rooms, String room){
+		for(Room r : rooms){
+			if(r.toString().equals(room)){
+				return r;
+			}
+		}
+		return null; //unreachable code
+	}
+	
+	@Override
+	public void toOutputSteam(DataOutputStream dout) throws IOException {
+//		private Position position;
+//		private String name;
+//		private String description;
+//		private Direction direction;
+		
+
+		dout.writeByte(Piece.PLAYER);
+		if(super.getPosition() == null){
+			dout.writeByte(0); //position is invalid (null)
+			dout.writeByte(-1);
+			dout.writeByte(-1);
+		}else{
+			dout.writeByte(1); //position is valid
+			dout.writeInt(super.getLocation().getxPos()); //send x location
+			dout.writeInt(super.getLocation().getyPos()); //send y location
+		}		
+		byte[] name = super.getName().getBytes("UTF-8");
+		dout.writeByte(name.length);
+		dout.write(name);
+		
+		byte[] desc = super.getDescription().getBytes("UTF-8");
+		dout.writeByte(desc.length);
+		dout.write(desc);
+		
+		dout.writeInt(super.getx()); //send RealX pos 
+		dout.writeInt(super.gety()); //send RealY pos
+		
+		byte[] fname = super.getImage().getBytes("UTF-8");
+		dout.writeInt(fname.length);
+		dout.write(fname); //send filename
+	
+		//----
+		dout.writeByte(super.getDirection().ordinal()); //send direction
+		
+		dout.writeInt(id); //write player id 
+		//write points
+		dout.writeByte(points);
+		
+		
+		//iterate over container
+		dout.writeByte(container.size());
+//		System.out.println("container.size()): " + container.size());
+		for(Piece p  : container){
+			p.toOutputSteam(dout);
+		}
+		
+		//iterate over assignments		
+		dout.writeByte(assignments.size());
+		for(Assignment a : assignments){
+			a.toOutputSteam(dout);
+		}		
 	}
 }
