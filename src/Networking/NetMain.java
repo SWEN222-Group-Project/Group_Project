3a270e39e9b023a5596fd84aa6c5446f7ed84f82
Miten Chauhan <Miten@Miten-HP-Laptop.vuw.ac.nz>
2015-10-07 10:08:44 +1300

    Created runServer method
    Enables the netMain class to run on server mode.

--- src/Networking/NetMain.java
+++ src/Networking/NetMain.java
@@ -1,8 +1,12 @@
 package Networking;
 
 import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 
+import model.Direction;
 import model.Game;
+import model.Player;
 
 public class NetMain {
 	
@@ -36,6 +40,30 @@ public class NetMain {
 
 	private static void runServer(int port, int nclients, Game game) {
 		// TODO Auto-generated method stub
+		System.out.println("SERVER LISTENING ON PORT " + port);
+		System.out.println("SERVER AWAITING " + nclients + " CLIENTS");
+		try {
+			Server[] connections = new Server[nclients];
+			// Now, we await connections.
+			ServerSocket ss = new ServerSocket(port);			
+			while (1 == 1) {
+				// 	Wait for a socket
+				Socket s = ss.accept();
+				System.out.println("ACCEPTED CONNECTION FROM: " + s.getInetAddress());				
+				System.out.println("ACCEPTED CONNECTION FROM: " + s.getInetAddress());				
+				int uid = id;				
+				connections[--nclients] = new Server(s,uid,game);
+				connections[nclients].start();				
+				if(nclients == 0) {
+					System.out.println("ALL CLIENTS ACCEPTED --- GAME BEGINS");					
+					System.out.println("ALL CLIENTS DISCONNECTED --- GAME OVER");
+					return; // done
+				}
+			}
+		} catch(IOException e) {
+			System.err.println("I/O error: " + e.getMessage());
+		} 
+
 		
 	}
 }
