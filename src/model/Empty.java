6cd2be5a0e93be0aad543de133a9178b4b697c73
Neel <Neel@Neels-laptop>
2015-10-09 01:01:22 +1300

    New class
    This new class is used when creating floor tiles in the game. The
    floortiles are rendered as an Empty object.

---
+++ src/model/Empty.java
@@ -0,0 +1,45 @@
+package model;
+
+import java.io.DataOutputStream;
+import java.io.IOException;
+
+public class Empty extends Piece{
+
+	public Empty(Position position, String name, String description,
+			Direction direction) {
+		super(position, "Empty", "", Direction.NORTH);
+		
+	}
+
+	@Override
+	public void toOutputStream(DataOutputStream dout) throws IOException {
+		dout.writeByte(Piece.EMPTY);
+		if(super.getPosition() == null){
+			dout.writeByte(0); //position is invalid (null)
+			dout.writeByte(-1);
+			dout.writeByte(-1);
+		}else{
+			dout.writeByte(1); //position is valid
+			dout.writeInt(super.getLocation().getxPos()); //send x location
+			dout.writeInt(super.getLocation().getyPos()); //send y location
+		}		
+		byte[] name = super.getName().getBytes("UTF-8");
+		dout.writeByte(name.length);
+		dout.write(name);
+		
+		byte[] desc = super.getDescription().getBytes("UTF-8");
+		dout.writeByte(desc.length);
+		dout.write(desc);
+		
+		dout.writeInt(super.getTileWidth());
+		dout.writeInt(super.getTileHeight());
+		dout.writeInt(super.getx()); //send RealX pos 
+		dout.writeInt(super.gety()); //send RealY pos
+		
+		byte[] fname = super.getImage().getBytes("UTF-8");
+		dout.writeInt(fname.length);
+		dout.write(fname); //send filename
+		
+	}
+
+}
