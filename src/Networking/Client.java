7828c3e58886f158799c0644cd88f3ada3fe105b
krinanagar <krinanagar@192.168.20.100>
2015-10-10 21:47:58 +1300

    Added comments and JavaDoc

--- src/Networking/Client.java
+++ src/Networking/Client.java
@@ -28,50 +28,40 @@ public class Client implements KeyListener{
 		this.socket = socket;
 		//this.game = game;
 	}
-
+	/**
+	 * Handles client functions, displays game based on server readings
+	 */
 	public void run() {
 		try {			
 			int i = 1;
+			//initialise data input and output streams
 			output = new DataOutputStream(socket.getOutputStream());
-			input = new DataInputStream(socket.getInputStream());
-			
-			System.out.println("reading from server:" + i);
+			input = new DataInputStream(socket.getInputStream());	
+			//read id from server
 			uid = input.readByte();
+			//create new game
 			Game game = new Game();
-
-//			byte[] gameByte = FileUtils.readFileToByteArray(Server.file);
-//			game.fromByteArray(gameByte);
+			//read board state from server
 			int stateLen = input.readInt();
 			byte[] data = new byte[stateLen];
 			input.readFully(data);
 			game.fromByteArray(data);
-			System.out.println("finished reading from server:" + i++);
+			//create new gameframe for client to see the game
 			GameFrame g = new GameFrame(game,uid);
 			g.addKeyListener(this);
-//			 new ClockThread(g).start();
 			System.out.println("Started game in client");
 			boolean exit=false;
 			byte[] ndata;
 			while(!exit) {
-				 
-//				byte[] ndata = FileUtils.readFileToByteArray(Server.file);
-//				game.fromByteArray(ndata);
-				System.out.println("reading from server:" + i);
+				//keep reading updates from server
 				stateLen = input.readInt();
 				ndata = new byte[stateLen];
 				input.readFully(ndata);
 				game.fromByteArray(ndata);
-				
-				System.out.println("finished reading from server:" + i++);
 				game.printAll();
-//				try {
-//					Thread.sleep(100);
-//				} catch (InterruptedException e) {
-//					//do nothing
-//				}
 
 			}
-			socket.close(); // release socket ... v.important!
+			socket.close(); // release socket
 		} catch(IOException e) {
 			System.err.println("I/O Error: " + e.getMessage());
 			e.printStackTrace(System.err);
@@ -87,39 +77,25 @@ public class Client implements KeyListener{
 
 	@Override
 	public void keyPressed(KeyEvent e) {
-		System.out.println("Client: 35");
 		try {
 			int code = e.getKeyCode();
-			
+			//send integer to server referring to key pressed direction
 			if(code == KeyEvent.VK_RIGHT || code == KeyEvent.VK_KP_RIGHT) {	
-				System.out.println("Client: RIGHT");
 				output.writeInt(3);
 				
 			} else if(code == KeyEvent.VK_LEFT || code == KeyEvent.VK_KP_LEFT) {	
-				System.out.println("Client: LEFT");
 				output.writeInt(4);
 				
 			} else if(code == KeyEvent.VK_UP) {	
-				System.out.println("Client: UP");
 				output.writeInt(1);
-				System.out.println("Client: written 69");
 				
 			} else if(code == KeyEvent.VK_DOWN) {	
-				System.out.println("Client: DOWN");
 				output.writeInt(2);
 				
 			}
 			output.flush();
-			//**** uncomment this to see that game does not change ***//
-
 			} catch(IOException ioe) {
-			// something went wrong trying to communicate the key press to the
-			// server.  So, we just ignore it.
 			} finally {
-//				System.out.println("Printing Game");
-//				game.printAll();
-//				g.repaint();
-//				System.out.println("repaint");
 			}
 	}
 
