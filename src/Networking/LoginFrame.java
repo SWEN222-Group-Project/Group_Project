5a0f20cce0ecaf47e33d6cd6c31ed5492e8510d8
krinanagar <krinanagar@192.168.20.100>
2015-10-13 19:51:20 +1300

    Add button functionalities
    Host and Join buttons are functional in order to do the job of changing
    variables in the NetMain class to run in client or server mode.

--- src/Networking/LoginFrame.java
+++ src/Networking/LoginFrame.java
@@ -2,10 +2,13 @@ package Networking;
 
 import java.awt.Dimension;
 import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 public class LoginFrame extends JFrame{
@@ -38,14 +41,52 @@ public class LoginFrame extends JFrame{
 		modeScreen.add(label);
 		//Create and add host button to panel
 		hostButton = new JButton("Host");
+		hostButton.addActionListener(new ActionListener(){
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				//update main variables to run in server mode
+				main.setServer(true);
+				main.setUrl(null);
+				//close mode selection 
+				setVisible(false);
+				//inform user server is running
+				JOptionPane.showMessageDialog(LoginFrame.this,"Server is running!");
+				//run server
+				main.start();
+			}
+		});
 		modeScreen.add(hostButton);
 		//Create and add join button to panel
 		joinButton = new JButton("Join");
+		joinButton.addActionListener(new ActionListener(){
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				//update main variables to run in client mode
+					main.setServer(false);
+					main.setUrl("localhost");
+					//close mode selection 
+					setVisible(false);
+					//remove current panel
+					remove(modeScreen);
+					//repaint login components
+					configure();
+			}
+		});
 		modeScreen.add(joinButton);
 		//set size of screen
 		setPreferredSize(new Dimension(350, 90));
 		//add panel to the frame
 		add(modeScreen);
+		
+	}
+
+	/**
+	 * Adds components to the frame requiring user to
+	 * input a username before starting the game in client
+	 * mode. 
+	 */
+	private void configure() {
+		// TODO Auto-generated method stub
 		
 	}
 
