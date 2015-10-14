06c3770191635db1fa50014ffc9ebb4daed0e10d
Harmannz <harman@harmans-mbp.lan>
2015-10-14 22:43:46 +1300

    Added Exceptions and Refactored Control class
    InvalidMove exception has been added to the control class that allows
    the InvalidMove exception to be thrown when a user attempts to make an
    invalid move such as moving on top of a player or attempting to drop
    items from an already empty player container. These exceptions are used
    to test the program through JUnit tests and also allow the game to catch
    and differentiate between errors and perform appropriate action.
    
    The class has been refactored and ready for the final merge.

--- src/control/Control.java
+++ src/control/Control.java
@@ -2,77 +2,105 @@ package control;
 
 import model.*;
 
+/**
+ * This class has methods that are called by the server to convey the user
+ * input into changes in the game state.
+ * This class performs error checks and changes the state of the game as appropriate.
+ *
+ * @author Harman (singhharm1)
+ *
+ */
 public class Control {
-	// game: model
-	private Game game;
-	
+
+	private Game game; //model
+
+	/**
+	 * Constructor: Creates the Control object.
+	 * @param game
+	 */
 	public Control(Game game){
 		this.game = game;
 	}
-	//TODO: need the fame class that is the view component.
-	
-	public boolean movePlayer(int playerId, Location destination, Room room){
-//		Position currentPos = game.getPlayer(playerId).getPosition();
-//		if(!currentPos.isAdjacentTo(destination)){
-//			//throw exception ("You can only move one square at a time.\nClick or use arrow keys.)
-//		}
-		
-//		Room currentRoom = currentPos.getRoom();
-		Piece piece = room.getPiece(destination);
-		
-		if(piece instanceof Player){
-			//throw exception("cannot jump onto another player!");
-			System.out.println("Cannot jump onto another player!");
-			return false;
+
+	/**
+	 * Moves a player to a the location in a given room.
+	 * If the move is invalid then no changes to the game state are made.
+	 *
+	 * @param playerId id of player to move
+	 * @param destination Location to move to
+	 * @param room Room to move to
+	 * @return true if the move is successful, false if move is not successful
+	 * @throws InvalidMove if the move is invalid
+	 */
+	public synchronized boolean movePlayer(int playerId, Location destination, Room room) throws InvalidMove{
+		Position currentPos = game.getPlayer(playerId).getPosition();
+		checkWon(playerId);
+		if(currentPos.getRoom() == room && !currentPos.isAdjacentTo(destination)){
+			throw new InvalidMove("You can only move one square at a time.\nUse arrow keys.");
 		}
+
+		Piece piece = room.getPiece(destination);
+		testPiece(piece); //test destination piece
+
 		if(piece instanceof Door){
 			System.out.println("Door move started");
 			doorMove(playerId, (Door)piece); //cast is safe
-			return false;
+			return false; //unsuccessful move
 		}
-		
+
 		if(piece != null){
 			game.pickItem(playerId, room, destination);
 			checkWon(playerId);
-			System.out.println("*Picking item at location " + destination);
-			return false;
+			return false; //unsuccessful move
 		}
-		System.out.println("moving player");
-		
+
 		game.movePlayer(playerId, destination, room); //move player
-		return true;
+		return true; //successful move
 	}
-	
-	private void doorMove(int playerId, Door door){
+
+	/**
+	 * Helper method to movePlayer. This method determines whether or not destination
+	 * of the move is valid.
+	 * Destination position is valid if it is not another player or a wall.
+	 * @param piece
+	 * @throws InvalidMove if move is invalid
+	 */
+	private void testPiece(Piece piece) throws InvalidMove{
+		if(piece instanceof Player){
+			throw new InvalidMove("cannot jump onto another player!");
+		}
+
+		if(piece instanceof Wall){
+			throw new InvalidMove("Cannot move through a wall");
+		}
+	}
+
+	/**
+	 * Helper method to movePlayer. This method is invoked if the player attempts
+	 * to move through a door.
+	 *
+	 * @param playerId
+	 * @param door
+	 * @throws InvalidMove if the door move is invalid
+	 */
+	private void doorMove(int playerId, Door door) throws InvalidMove{
 		//check if door is unlocked.
-		//if unlocked then simply move player
-		//if locked check if any of the keys of the player's container open the lock
-		//if so then move the player and unlock the door
-		//otherwise return
+		//if unlocked then simply move player.
+		//if locked check if any of the keys of the player's container open the lock.
 		Player player = game.getPlayer(playerId);
 		Room oldRoom = player.getRoom();
 		Location oldLoc = player.getLocation();
-		
+
 		Position newPosition = null;
-		System.out.println("Moving through door");
 		if(!door.isLocked()){
-//			//attempt to move player if return true then ok otherwise revert to old pos
-			Position doorPos = door.leadsTo();	
-//			game.movePlayer(playerId, door.leadsTo());
-//			player.getPosition().setRoom(doorPos.getRoom());
-//			if(!movePlayer(playerId, doorPos.getLocation())){
-//				//if move failed then revert to old position
-//				System.out.println("Door move failed");
-//				player.getPosition().setRoom(oldPos.getRoom());
-//				movePlayer(playerId, oldPos.getLocation());
-//			}
-			
+			//attempt to move player through the unlocked door
+			Position doorPos = door.leadsTo();
 			if(movePlayer(playerId, doorPos.getLocation(), doorPos.getRoom())){
-				if(oldRoom == null) System.out.println("oldroom == null");
 				oldRoom.removePiece(oldLoc);
 			}
-//			game.movePlayer(playerId, door.leadsTo());
+
 		}else{
+			//attempt to unlock the door by checking if the player possess the keys.
 			for(Piece p : player.container()){
 				if(p instanceof Key && door.canEnter((Key) p)){
 					newPosition = door.leadsTo();
@@ -81,37 +109,39 @@ public class Control {
 			}
 			Position doorPos = door.leadsTo();
 			if(newPosition != null){
-				//game.movePlayer(playerId, newPosition);
-				System.out.println(doorPos.getLocation());
+				//move player through the door
 				if(movePlayer(playerId, doorPos.getLocation(), doorPos.getRoom())){
-					if(oldRoom == null) System.out.println("oldroom == null");
 					oldRoom.removePiece(oldLoc);
 				}
-//				this.movePlayer(playerId, newPosition); 
 			}
 		}
-		
-		
-//		System.out.println(game.getPlayer(playerId).getPosition());
 	}
-	
-	
-	private void checkWon(int playerId){
-//		if(playerId)
+
+	/**
+	 * Determines whether or not a player has won.
+	 * If the player has won then the game must be notified.
+	 * @param playerId
+	 */
+	public void checkWon(int playerId){
 		if(game.getPlayer(playerId).hasWon()){
 			game.hasWon(playerId);
 		}
 	}
-	public void printAll(){
+
+	/**
+	 * Prints a textual representation of the game
+	 */
+	public synchronized void printAll(){
 		game.printAll();
 	}
-	public String printRoom(Room room){
-		return room.printRoom();
-	}
-	
-	//examine item(room, location) based on user click
-	public String examineItem(int playerId, Location interest){
-		//need to return the description of the item at interset location, iff not null;
+
+	/**
+	 * Returns the description of the item located at the Location given.
+	 * @param playerId
+	 * @param interest
+	 * @return Description of item if present
+	 */
+	public synchronized String examineItem(int playerId, Location interest){
 		Room currentRoom = game.getPlayer(playerId).getRoom();
 		Piece piece = currentRoom.getPiece(interest);
 		if(piece != null){
@@ -120,26 +150,37 @@ public class Control {
 			return "There is no item at location: " + interest ;
 		}
 	}
-	
+
 	/**
-	 * Player (playerId) drops item
+	 * This method drops the last added Item in the player's container.
+	 * The item is dropped on the nearest adjacent space to the player
+	 *
 	 * @param playerId
-	 * @param item
+	 * @throws InvalidMove if there are no items to drop or no empty adjacent space
 	 */
-	public void dropItem(int playerId, Piece item){
-		//first get empty location
+	public synchronized void dropItem(int playerId) throws InvalidMove{
 		Player player = game.getPlayer(playerId);
 		if(!player.hasItems()){
-			System.out.println("there are no items to drop");
-			return;
-			//throw exception("there are no items to drop");
+			throw new InvalidMove("There are no items to drop in your container");
 		}
+		//first get empty location to drop item to
 		Location emptySpace = game.getAdjacentSpace(player.getPosition());
 		if(emptySpace == null){
-			//throw exception("there are no empty adjacent space to drop item");
-			System.out.println("there are no empty adjacent space to drop item");
-			return;
+			throw new InvalidMove("there are no empty adjacent space to drop item");
+
 		}
-		game.dropItem(playerId, item, emptySpace);	
+		game.dropItem(playerId, emptySpace);
 	}
+
+
+	/**
+	 * Indicates an attempt to make an invalid move.
+	 *
+	 */
+	public static class InvalidMove extends Exception {
+		public InvalidMove(String msg) {
+			super(msg);
+		}
+	}
+
 }
