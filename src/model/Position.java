8ac63f69442664aefb7797244b5d2b498739e2a4
Harmannz <harman@harmans-mbp.lan>
2015-10-14 23:02:07 +1300

    Added equals and hashCode and refactored class
    equals and hashCode methods have been added, this is mainly for junit
    tests as they involve comparing positions. The position objects are not
    compared or checked for equality in the rest of the program so they
    should not cause any unexpected behaviour. 
    Javadocs and comments have been added to ensure that this class is
    readable.
    The Position class is ready for the final merge.

--- src/model/Position.java
+++ src/model/Position.java
@@ -1,35 +1,102 @@
 package model;
+
 /**
- * TODO: 
- * @author harman
- *
+ * This class is used to store Room and the Location a Piece in the game.
+ * This class contains the Room that the piece is in and also
+ * the Location of the piece inside the room
+ * @author Harman (singhharm1) 
  */
 public class Position {
 	private Room room;
 	private Location location;
 	
+	/**
+	 * Constructor: Takes in the position of where the Assignment is to be placed in the game
+	 * and the id (number) of the assignment and a description of the assignments to be displayed
+	 * on the player's container.
+	 * @param room
+	 * @param location
+	 */
 	public Position(Room room, Location location){
 		this.room = room;
 		this.location = location;
 	}
 	
+	/**
+	 * Returns the room.
+	 * @return
+	 */
 	public Room getRoom(){
 		return this.room;
 	}
 	
+	/**
+	 * Sets the room
+	 * @param room
+	 */
 	public void setRoom(Room room){
 		this.room = room;
 	}
 	
+	/**
+	 * Returns the Location as a separate object.
+	 *  
+	 * @return
+	 */
 	public Location getLocation(){
 		return new Location(location.getxPos(), location.getyPos());
 	}
 	
+	/**
+	 * Sets the Location, perform a defensive copy
+	 * @param loc
+	 */
 	public void setLocation(Location loc){
 		this.location = new Location(loc.getxPos(), loc.getyPos());
 	}
 
+	/**
+	 * Determines whether or not the newLocation is adjacent to the current location.
+	 * This method is ideally used to compare two locations within the same room.
+	 * @param newLocation
+	 * @return true if location is adjacent to the current location, false otherwise
+	 * @author Harman (singhharm1)
+	 */
 	public boolean isAdjacentTo(Location newLocation) {
 		return this.location.isNextTo(newLocation);
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((location == null) ? 0 : location.hashCode());
+		result = prime * result + ((room == null) ? 0 : room.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Position other = (Position) obj;
+		if (location == null) {
+			if (other.location != null)
+				return false;
+		} else if (!location.equals(other.location))
+			return false;
+		if (room == null) {
+			if (other.room != null)
+				return false;
+		} else if (!room.equals(other.room))
+			return false;
+		return true;
+	}
+	
+	
 }
